4da44b268976285915b8f8708bf58d34eea1321f
Andrew TOwnsley <atownsley86@gmail.com>
2022-05-26 17:33:10 -0600

    BUilding out basic todo functionality.

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
-$( document ).ready(function() {
-    console.log( 'Does it Work??' );
-  });
+  jQuery(function() {
+      // Create new Appt with First Name on BTN CLick
+      $('#addApptBtn').on('click', () => {
+          console.log("addApptBtn Clicked!");
+      })
+        // Check if inout has a value
+      // Add to Appt Array
+      // map over appt array and append to appt div
+  })
